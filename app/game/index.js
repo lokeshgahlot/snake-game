07d757fa51857bfda5ac7706d7d3545d99182894
Lokesh <lokesh.gahlot@on24.com>
2017-09-26 14:49:36 -0700

    Work in progress modified:   app/game/index.js

--- app/game/index.js
+++ app/game/index.js
@@ -8,8 +8,10 @@ import showModal  from '../utils/modal';
 import {drawCell, getClientWidth, getClientHeight, getRandomPos} from '../utils';
 
 const BOARD_COLOR = 'papayawhip';
+const SNAKE_COLOR = 'blue';
+const FOOD_COLOR = 'tomato';
 const SCALE = 20;
-const FRAMES_PER_SECOND = 15;
+const FRAMES_PER_SECOND = 15; // It can be use as speed also.
 
 export default () => {
   const boardEl = document.querySelector('.board');
@@ -18,43 +20,49 @@ export default () => {
   const board = setupBoard(boardEl, ctx, SCALE);
   const totalRow = board.width / SCALE;
   const totalColumn = board.height / SCALE;
-  const snake = Snake({ drawCell: drawCell(ctx, 'blue', SCALE)});
-  const food = Food({ drawCell: drawCell(ctx, 'tomato', SCALE)});
+  const snake = Snake({ drawCell: drawCell(ctx, SNAKE_COLOR, SCALE)});
+  const food = Food({ drawCell: drawCell(ctx, FOOD_COLOR, SCALE)});
   const controls = Controls();
   let score = 0, interval = 1000/FRAMES_PER_SECOND;
-  let boardPosition, timerId;
+  let foodPosition, timerId;
 
+  // run method paints the canvas
   const run = () => {
     board.draw(BOARD_COLOR);
     snake.move().draw();
     const {x, y} = snake.getHead();
-    //
+
+    // Wall and snake body collision conditions
     if (x < 0 || x >= totalRow || y < 0 || y >= totalColumn || snake.checkBobyCollision()) {
       clearTimeout(timerId);
       stop();
       return;
     }
-    if (snake.eat(...boardPosition)) {
-      boardPosition = getRandomPos(totalRow, totalColumn);
+
+    // check if snake eats the food
+    if (snake.eat(...foodPosition)) {
+      foodPosition = getRandomPos(totalRow, totalColumn);
       score += 5;
       scoreEl.innerHTML = CONSTANTS.scoreLabel + ' ' + score;
     }
-    food.draw(...boardPosition);
+    food.draw(...foodPosition);
     timerId = setTimeout(run, interval);
   }
 
+  // game starts here
   const start = () => {
     score = 0;
     scoreEl.innerHTML = CONSTANTS.scoreLabel + ' ' + score;
-    snake.reset();
     controls.add(snake);
-    boardPosition = getRandomPos(totalRow, totalColumn);
+    foodPosition = getRandomPos(totalRow, totalColumn);
     run();
   }
 
+  // game stops here
   const stop = () => {
     clearTimeout(timerId);
     controls.remove(snake);
+    snake.reset();
     showModal({
       header: CONSTANTS.headerMsg,
       msg: CONSTANTS.gameOverMsg,
